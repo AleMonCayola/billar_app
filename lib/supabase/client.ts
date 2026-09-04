970d8698345f578755f8be95ef12962f8ebf5525
Alejandro Montaño Cayola <145299971+AleMonCayola@users.noreply.github.com>
2026-09-04 08:55:27 -0400

    Implement singleton pattern for Supabase client
    Refactor createClient to use a singleton pattern for SupabaseClient.

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,14 @@
 import { createBrowserClient } from "@supabase/ssr";
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+let cliente: SupabaseClient | undefined;
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  if (!cliente) {
+    cliente = createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  }
+  return cliente;
 }
